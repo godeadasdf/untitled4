ef4d986d46eda81cae98ca1b245ed17626adb13c
godeadasdf <793356239@qq.com>
2016-05-13 19:48:16 +0800

    newbie id->item_id view arguments

--- wanna_buy_save_first/urls.py
+++ wanna_buy_save_first/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^(?P<id>[0-9]+)/$', views.detail, name='detail')
+    url(r'^(?P<item_id>[0-9]+)/$', views.detail, name='detail')
 ]
