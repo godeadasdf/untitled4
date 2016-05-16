6f7605048dd56688ae3b00e3b49522c0d03a9ba2
godeadasdf <793356239@qq.com>
2016-05-16 19:26:15 +0800

    add login and learn how to operate session

--- wanna_buy_save_first/urls.py
+++ wanna_buy_save_first/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^(?P<item_id>[0-9]+)/$', views.detail, name='detail'),
-    url(r'^login/$', views.login, name='login')
+    url(r'^login/$', views.login, name='login'),
+    url(r'^home/(?P<user_id>[0-9]+)/$', views.home, name='home'),
 ]
--- wanna_buy_save_first/views.py
+++ wanna_buy_save_first/views.py
@@ -1,26 +1,50 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, render_to_response
+from django.http import HttpResponse, HttpResponseRedirect
 from .models import Item, User
+
+app_name = 'wanna_buy_save_first/'
 
 
 def index(request):
     items = Item.objects.all()
     context = {'items': items}
-    return render(request, 'wanna_buy_save_first/index.html', context)
+    return render(request, app_name + 'index.html', context)
 
 
 def detail(request, item_id):
     if not request.session.get('user', False):
-        return render(request, 'wanna_buy_save_first/login.html')
+        return render(request, app_name + 'login.html')
     item = Item.objects.get(pk=item_id)
     context = {'item': item}
-    return render(request, 'wanna_buy_save_first/detail.html', context)
+    return render(request, app_name + 'detail.html', context)
 
 
 def login(request):
-    m = User.objects.get(name=request.POST['username'])
-    if m.password == request.POST['password']:
-        request.session['user'] = m.id
-        return HttpResponse("You're logged in")
+    try:
+        m = User.objects.get(name=request.POST['username'])
+    except:
+        return render(request, app_name + 'login.html')
     else:
-        return HttpResponse("Your name and password didn't match")
+        if m.password == request.POST['password']:
+            request.session['user'] = m.id
+            return HttpResponseRedirect(request.META.get('HTTP_REFERER', '/'))
+        else:
+            return HttpResponse("Your name and password didn't match")
+
+
+def home(request, user_id):
+    user = User.objects.get(id=user_id)
+    context = {'user': user}
+    if not request.session.get('user', False):
+        return render(request, app_name + 'login.html')
+    else:
+        if int(request.session['user']) == int(user_id):
+            context['session'] = request.session['user']
+            context['req'] = user_id
+            context['belong'] = 'me'
+            return render(request, app_name + 'home.html', context)
+        else:
+            context['session'] = request.session['user']
+            context['req'] = user_id
+            context['belong'] = 'other'
+            return render(request, app_name + 'home.html', context)
